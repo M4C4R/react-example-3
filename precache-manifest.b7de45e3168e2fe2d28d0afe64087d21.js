08436b90d0c66b81a76ab9bdeceacc577b751180
Acar, Mert <mert.acar@sky.uk>
2020-02-11 14:37:25 +0000

    Updates

---
+++ precache-manifest.b7de45e3168e2fe2d28d0afe64087d21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdea3e809d07cd4a7acdbf3ca8f746b7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68d9661847bb4df64c80",
+    "url": "/static/css/main.64cee466.chunk.css"
+  },
+  {
+    "revision": "b32b63f50e3667e66d11",
+    "url": "/static/js/2.d525c01e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.d525c01e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68d9661847bb4df64c80",
+    "url": "/static/js/main.029ea2b1.chunk.js"
+  },
+  {
+    "revision": "6b378cb959e2e0c738b2",
+    "url": "/static/js/runtime-main.3f34763b.js"
+  }
+]);
